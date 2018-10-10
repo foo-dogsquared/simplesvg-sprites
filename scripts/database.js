aac97cc805fd11174d52135a060a6987866db67a
foo-dogsquared <christiangabrielarazas@gmail.com>
2018-10-10 18:38:51 +0800

    Update the server to accept POST requests to '/compile' (in testing phase)

--- scripts/database.js
+++ scripts/database.js
@@ -4,12 +4,12 @@ const simple_icons = require('simple-icons');
 const icons_list = Array.from(Object.keys(simple_icons));
 
 icons_list.sort(function (current, next) {
-    let _current = current.slice(0, 1).toLowerCase();
-    let _next = next.slice(0, 1).toLowerCase();
+    let _current = current.toLowerCase();
+    let _next = next.toLowerCase();
 
-    if (_current === _next) return 0
-    else if (_current > _next) return 1
-    else return -1;
+    if (_current > _next) return 1
+    else if (_current < _next) return -1
+    else return 0;
 })
 
 module.exports = icons_list;
--- scripts/server.js
+++ scripts/server.js
@@ -3,6 +3,7 @@ const api_operations = require('./api');
 const express = require('express');
 const fs = require('fs');
 const SI_JSON = require('./database');
+const body_parser = require('body-parser')
 
 // initialize the variables
 const port = process.env.PORT || 3000;
@@ -12,8 +13,10 @@ function init(public_dir) {
     // start the server to listen for requests now
     app
         .use(express.static(public_dir))
+        .use(body_parser.urlencoded({extended: true}))
         .get('/simple-icons', api_operations.icon_get)
         .get('/database', function(req, res) {res.status(200).json(SI_JSON)})
+        .post('/compile', function(req, res) {res.status(200).json({response: "OK"})})
         .listen(port, () => {
             console.log(`Server opened up at port ${port}.`)        
         })
